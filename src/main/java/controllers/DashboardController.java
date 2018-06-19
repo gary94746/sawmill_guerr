b632883aa6758b54627c2bacc834bb394a5b103c
edgar chavez <edgar.109885@gmail.com>
2018-06-18 22:29:44 -0500

    some

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
@@ -10,17 +11,31 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class DashboardController {
-
+public class DashboardController implements Initializable {
 
     @FXML
     private BorderPane borderDashboard;
 
+    //Parent
+    private Parent parentResumen;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            parentResumen = FXMLLoader.load(getClass().getResource("/views/resumen.fxml"));
+            borderDashboard.setCenter(parentResumen);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @FXML
     void setResumen(MouseEvent event) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/views/resumen.fxml"));
-        borderDashboard.setCenter(parent);
+        borderDashboard.setCenter(parentResumen);
     }
 
     @FXML
